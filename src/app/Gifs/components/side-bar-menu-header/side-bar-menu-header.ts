d985759b6b26313b423d08ee9348ef3b401cce7d
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-21 17:10:04 -0600

    feat: create an environment

--- src/app/Gifs/components/side-bar-menu-header/side-bar-menu-header.ts
+++ src/app/Gifs/components/side-bar-menu-header/side-bar-menu-header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'side-bar-menu-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   templateUrl: './side-bar-menu-header.html',
 })
 export class SideBarMenuHeader {
-
+  envs = environment
 }
