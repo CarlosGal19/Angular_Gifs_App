b237de128ffe4fcf1d0f0d521edf5264a895936b
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-22 23:39:41 -0600

    feat: auto fetch images when scroll almost finishes

--- src/app/Gifs/Pages/trending-page/trending-page.ts
+++ src/app/Gifs/Pages/trending-page/trending-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, viewChild } from '@angular/core';
 import { GifList } from "../../components/gif-list/gif-list";
 import { Gifs } from '../../services/gifs.service';
 
@@ -9,4 +9,23 @@ import { Gifs } from '../../services/gifs.service';
 })
 export default class TrendingPage {
   gifService = inject(Gifs);
+
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv')
+
+  @HostListener('scroll', ['$event'])
+  onScroll(event: Event) {
+    // const scrollOffset = (event.target as HTMLElement).scrollTop;
+    // console.log('Scroll position:', scrollOffset);
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+
+    if (!scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop;        // CURRENT SCROLL LEVEL
+    const clientHeight = scrollDiv.clientHeight;  // SIZE OF SCREEN
+    const contentSize = scrollDiv.scrollHeight;   // MAXIMU HTML ELEMENT SIZE;
+    const totalScroll = scrollTop + clientHeight + 300; // LIMIT WITH 300px OF MARGIN
+    if (totalScroll >= contentSize) {
+      this.gifService.loadTrendingGifs()
+    }
+  }
 }
